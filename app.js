5bdb407966c80c8979424005ae976dca730355ac
Starlight Romero <starlightromero@gmail.com>
2021-01-20 15:40:23 -0800

    [update] '/' route to log

--- app.js
+++ app.js
@@ -23,10 +23,8 @@ app.get('/:username', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-  // set the url of the gif
-  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-  // render the hello-gif view, passing the gifUrl into the view to be displayed
-  res.render('hello-gif', { gifUrl })
+  console.log(req.query)
+  res.render('home')
 })
 
 // Start Server
